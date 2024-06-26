1059236771ab9141d6624e24e3a8daec98219369
umer-shehzad <umer.work.96@gmail.com>
2024-06-26 20:54:55 +0500

    code fix in password form page

--- src/components/signup/PasswordForm.jsx
+++ src/components/signup/PasswordForm.jsx
@@ -1,32 +1,28 @@
-import React from 'react';
-import { Button, TextField, Typography, Box, Container } from '@mui/material';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
-import { Height } from '@mui/icons-material';
-import InputField from '../common/InputField';
+import React, { useState } from 'react';
+import { Box, Typography, TextField, IconButton, InputAdornment } from '@mui/material';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+
 import CustomButton from '../common/CustomButton';
 
-// const SignupSchema = Yup.object().shape({
-//     userName: Yup.string()
-//         .required('User Name is required'),
-//     email: Yup.string()
-//         .email('Invalid email')
-//         .required('Email is required'),
-//     contact: Yup.string()
-//         .required('Contact is required'),
-//     gender: Yup.string()
-//         .required('Gender is required'),
-//     age: Yup.number()
-//         .required('Age is required')
-//         .positive('Age must be a positive number')
-//         .integer('Age must be an integer'),
-// });
+
 
 const PasswordForm = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+    const handleTogglePassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleToggleConfirmPassword = () => {
+        setShowConfirmPassword(!showConfirmPassword);
+    };
+
     return (
-        <Box display={'flex'} flexDirection={'column'} rowGap={5}>
-            {/* logo */}
-            <Box display={'flex'} alignItems={'center'} justifyContent={'center'}>
+        <Box display="flex" flexDirection="column" rowGap={5}>
+            {/* Logo */}
+            <Box display="flex" alignItems="center" justifyContent="center">
                 <Box
                     component="img"
                     sx={{ width: '65%' }}
@@ -35,25 +31,108 @@ const PasswordForm = () => {
                 />
             </Box>
 
-            {/* form */}
+            {/* Form */}
             <Formik
                 initialValues={{
                     password: '',
                     confirmPassword: '',
                 }}
-                // validationSchema={SignupSchema}
                 onSubmit={(values) => {
                     console.log(values);
                 }}
             >
                 {({ errors, touched }) => (
                     <Form>
-                        <Box display={'flex'} flexDirection={'column'} rowGap={2}>
+                        <Box display="flex" flexDirection="column" rowGap={2}>
 
-                            <InputField labelName="Password" fieldName="password" />
-                            <InputField labelName="Confirm Password" fieldName="confirmPassword" />
+                            {/* Password Field */}
+                            <Box>
+                                <Typography variant="body2" mb={0.5}>Password</Typography>
+                                <Field
+                                    name="password"
+                                    as={TextField}
+                                    variant="outlined"
+                                    fullWidth
+                                    type={showPassword ? 'text' : 'password'}
+                                    // error={!!errors['password'] && touched['password']}
+                                    // helperText={<ErrorMessage name="password" />}
+                                    sx={{
+                                        '& .MuiOutlinedInput-root': {
+                                            borderRadius: '8px',
+                                            height: '42px',
+                                            fontSize: showPassword ? '' : '2rem'
+                                        }
+                                    }}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    onClick={handleTogglePassword}
+                                                    edge="end"
+                                                    size="large"
+                                                    sx={{
+                                                        color: 'black',
+                                                        '&:hover': {
+                                                            backgroundColor: 'transparent',
+                                                        },
+                                                        '& svg': {
+                                                            fontSize: '1.5rem'
+                                                        }
+                                                    }}
+                                                >
+                                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            </Box>
 
-                            <CustomButton btnName={'Continue'} />
+                            {/* Confirm Password Field */}
+                            <Box>
+                                <Typography variant="body2" mb={0.5}>Confirm Password</Typography>
+                                <Field
+                                    name="confirmPassword"
+                                    as={TextField}
+                                    variant="outlined"
+                                    fullWidth
+                                    type={showConfirmPassword ? 'text' : 'password'}
+                                    // error={!!errors['confirmPassword'] && touched['confirmPassword']}
+                                    // helperText={<ErrorMessage name="confirmPassword" />}
+                                    sx= {{
+                                        '& .MuiOutlinedInput-root': {
+                                            borderRadius: '8px',
+                                            height: '42px',
+                                            fontSize: showConfirmPassword ? '' : '2rem'
+                                        }
+                                    }}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    onClick={handleToggleConfirmPassword}
+                                                    edge="end"
+                                                    size="large"
+                                                    sx={{
+                                                        color: 'black',
+                                                        '&:hover': {
+                                                            backgroundColor: 'transparent',
+                                                        },
+                                                        '& svg': {
+                                                            fontSize: '1.5rem'
+                                                        }
+                                                    }}
+                                                >
+                                                    {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                                                </IconButton>
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                            </Box>
+
+                            {/* Continue Button */}
+                            <CustomButton btnName="Continue" />
                         </Box>
                     </Form>
                 )}
